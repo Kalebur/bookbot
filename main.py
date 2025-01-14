401b91501b1e38c265c6d4cd8999a59405121dfd
James Smith <kaleburdev@gmail.com>
2025-01-13 22:31:11 -0500

    Add function to get a dictionary of book characters with counts

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 def main():
     book_path = "./books/frankenstein.txt"
     book_text = get_book_text(book_path)
-    word_count = get_word_count(book_text)
-    print(f"There are {word_count} words in the book.")
+    book_character_dict = get_character_counts(book_text)
+    print(book_character_dict)
 
 def get_word_count(text):
     words = text.split()
     return len(words)
+
+def get_character_counts(text):
+    character_counts = {}
+    for char in text:
+        lowercase = char.lower()
+        if lowercase in character_counts:
+            character_counts[lowercase] += 1
+        else:
+            character_counts[lowercase] = 1
+
+    return character_counts
 
 def get_book_text(path):
     with open(path) as f:
