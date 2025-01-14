b4db9895c789c1610129e6b1546154a7675f1e30
James Smith <kaleburdev@gmail.com>
2025-01-13 22:48:52 -0500

    Add a function to print a report of word and character counts in a book

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import string
+
 def main():
-    book_path = "./books/frankenstein.txt"
+    book_name = "frankenstein.txt"
+    book_path = f"books/{book_name}"
     book_text = get_book_text(book_path)
     book_character_dict = get_character_counts(book_text)
-    print(book_character_dict)
+    print_character_report(book_character_dict, book_path, get_word_count(book_text))
 
 def get_word_count(text):
     words = text.split()
@@ -24,5 +27,20 @@ def get_book_text(path):
         file_contents = f.read()
 
         return file_contents
+
+def print_character_report(character_dict, book_path, word_count):
+    letters = list(string.ascii_lowercase)
+    sorted_characters = sorted(character_dict.items(), key=lambda x: x[1], reverse=True)
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{word_count} words found in the book.")
+    print("")
+    for item in sorted_characters:
+        char, count = item
+        if char not in letters:
+            continue
+        print(f"The '{char}' character was found {count} times.")
+
+    print("--- End report ---")
+
 
 main()
